9831a56298e30cab1be59dd4946d0847de967ad5
JONATHAN <joncarajoinas@gmail.com>
2021-04-23 09:49:26 +0100

    login exemplo

--- loginexemplo.php
+++ loginexemplo.php
@@ -4,4 +4,10 @@ $_SESSION['id']=1;
 $_SESSION['nome']='Jonathan';
 ?>
 
-<button onclick="location:index.php?id=<?php echo $_SESSION['id']?>"></button>
+<button><a href="index.php">Login</a> </button>
+
+<script src="../js/jquery.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+<script src="../js/owl.carousel.min.js"></script>
+<script src="../js/smoothscroll.js"></script>
+<script src="../js/custom.js"></script>
