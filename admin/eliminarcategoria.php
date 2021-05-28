deb2a9fd30c91aa12e9a04bacac25d239996b1da
JonathanCaracol <joncarajoinas@gmail.com>
2021-05-28 16:48:59 +0100

    minimo

--- admin/eliminarcategoria.php
+++ admin/eliminarcategoria.php
@@ -3,7 +3,13 @@ $id=intval($_GET['id']);
 include_once ("../includes/config.inc.php");
 $con=mysqli_connect(HOST,USER,PASSWORD,DATABASE);
 $sql= "delete from categorias where categoriaId=".$id;
+$sql1= "delete from servicos where servicoCategoriaId=".$id;
+$sql2= "delete from avaliacao inner join servicos on avaliacaoServicoId=servicoId where servicoCategoriaId=".$id;
+$sql3= "delete from pedidos inner join servicos on pedidoServicoId=servicoId where servicoCategoriaId=".$id;
 
+mysqli_query($con,$sql2);
+mysqli_query($con,$sql3);
+mysqli_query($con,$sql1);
 mysqli_query($con,$sql);
 header("location:../admin/offersAdmin.php");
 ?>
