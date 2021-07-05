16061aba8b9428d4caf5d876a8c95fbde88a33f6
JonathanCaracol <joncarajoinas@gmail.com>
2021-07-05 16:42:12 +0100

    criação da página slideBackOffice.php

--- admin/slideBackOffice.php
+++ admin/slideBackOffice.php
@@ -3,7 +3,7 @@ include_once("../includes/body.inc.php");
 topAJAXAdmin(MAIS);
 
 
-$sql ="Select * from categorias";
+$sql ="Select * from slideshow";
 $result=mysqli_query($con,$sql);
 ?>
 
@@ -21,12 +21,12 @@ $result=mysqli_query($con,$sql);
             <tr>
                 <td colspan="4"  align='center' style="vertical-align: middle">
 
-                    <a class="btn-sm btn-success" href="../admin/adicionaCategorias.php">Adicionar nova categoria</a>
+                    <a class="btn-sm btn-success" href="../admin/adicionaSlideShow.php">Adicionar nova imagem</a>
                 </td>
             </tr>
             <tr>
                 <th width="40%">Imagem</th>
-                <th width="40%">Nome da categoria</th>
+                <th width="40%">Id da imagem</th>
                 <th colspan="2" style="text-align: center">Opções</th>
             </tr>
 
@@ -34,10 +34,10 @@ $result=mysqli_query($con,$sql);
             while($dados=mysqli_fetch_array($result)){// enquanto existirem registos no result
                 ?>
                 <tr>
-                    <td><img width="120" src="../<?php echo $dados['categoriaImagemURL']?>"></td>
-                    <td><?php echo $dados['categoriaNome']?></td>
-                    <td align="center"><a href="editarcategoria.php?id=<?php echo $dados['categoriaId']?>"><svg xmlns="http://www.w3.org/2000/svg" color="green" width="26" height="26" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16"><path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/><path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/></svg></a></td>
-                    <td align="center"><a onclick="confirmaElimina(<?php echo $dados['categoriaId'] ?>)"><svg xmlns="http://www.w3.org/2000/svg" color="green" width="26" height="26" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/><path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/></svg></a></td>
+                    <td><img width="120" src="../<?php echo $dados['UrlSlideShow']?>"></td>
+                    <td><?php echo $dados['idslideShow']?></td>
+                    <td align="center"><a href="editarSlideShow.php?id=<?php echo $dados['idslideShow']?>"><svg xmlns="http://www.w3.org/2000/svg" color="green" width="26" height="26" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16"><path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/><path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/></svg></a></td>
+                    <td align="center"><a onclick="confirmaElimina(<?php echo $dados['idslideShow'] ?>)"><svg xmlns="http://www.w3.org/2000/svg" color="green" width="26" height="26" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/><path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/></svg></a></td>
                 </tr>
                 <?php
             }
@@ -47,8 +47,8 @@ $result=mysqli_query($con,$sql);
 </section>
 <script>
     function confirmaElimina(id){
-        if (confirm('Tem a certeza que quer eliminar a categoria?')) {
-            window.location.href ="eliminarcategoria.php?id="+id;
+        if (confirm('Tem a certeza que quer eliminar esta imagem?')) {
+            window.location.href ="eliminarSlideShow.php?id="+id;
         }
     }
 </script>
